e127ac9514e64ac9ecd57ec919b9f7b04335f2cc
aasedek <58518345+aasedek@users.noreply.github.com>
2024-09-23 15:05:05 +0800

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_NAME = "ChatWizard"
+const APP_DEFAULT_TITLE = "ChatWizard"
+const APP_TITLE_TEMPLATE = "%s - ChatWizard"
+const APP_DESCRIPTION = "ChatWizard PWA!"
 
 interface RootLayoutProps {
   children: ReactNode
